010612f6038a6dc84bf266ceff6891a155e0ce10
Victor Gabriel <victorgabrielcc22@gmail.com>
2025-07-24 11:13:21 -0300

    feat: exception and service

--- WebShop/backend/src/main/java/com/example/WebShop/mapper/ProdutoMapper.java
+++ WebShop/backend/src/main/java/com/example/WebShop/mapper/ProdutoMapper.java
@@ -1,6 +1,23 @@
 package com.example.WebShop.mapper;
 
+import java.util.List;
 
+import org.mapstruct.*;
+
+import com.example.WebShop.dto.produto.ProdutoRequestDto;
+import com.example.WebShop.dto.produto.ProdutoResponseDto;
+import com.example.WebShop.entities.Produto;
+
+@Mapper(componentModel = "spring")
 public interface ProdutoMapper {
+    
+    ProdutoResponseDto toDto(Produto entity);
+
+    Produto toEntity(ProdutoRequestDto dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDto(ProdutoRequestDto dto, @MappingTarget Produto entity);
+
+    List<ProdutoResponseDto> toDtoList(List<Produto> entities);
     
 }
